4592ce3f80b1f5d8f0ff0d6e84ee8371c7112a34
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-12-13 13:54:39 +0000

    Added a setup

--- exercise/day04/src/test/java/blog/ArticleTests.java
+++ exercise/day04/src/test/java/blog/ArticleTests.java
@@ -1,28 +1,28 @@
 package blog;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ArticleTests {
-    @Test
-    void it_should_add_valid_comment() throws CommentAlreadyExistException {
-        var article = new Article(
+    private Article article;
+
+    @BeforeEach
+    void setup(){
+        article = new Article(
                 "Lorem Ipsum",
                 "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
         );
-
+    }
+    @Test
+    void it_should_add_valid_comment() throws CommentAlreadyExistException {
         article.addComment("Amazing article !!!", "Pablo Escobar");
     }
 
     @Test
     void it_should_add_a_comment_with_the_given_text() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
         var text = "Amazing article !!!";
         article.addComment(text, "Pablo Escobar");
 
@@ -33,11 +33,6 @@ class ArticleTests {
 
     @Test
     void it_should_add_a_comment_with_the_given_author() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
         var author = "Pablo Escobar";
         article.addComment("Amazing article !!!", author);
 
@@ -48,20 +43,12 @@ class ArticleTests {
 
     @Test
     void it_should_add_a_comment_with_the_date_of_the_day() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
         article.addComment("Amazing article !!!", "Pablo Escobar");
     }
 
     @Test
     void it_should_throw_an_exception_when_adding_existing_comment() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+
         article.addComment("Amazing article !!!", "Pablo Escobar");
 
         assertThatThrownBy(() -> {
